125de69eda4e139a71ca230e8ff5e7ca2fa33e8c
IanW <ian.washington234@gmail.com>
2019-02-24 15:22:51 -0800

    More experimenting
    Trying to figure out how to store the word inputs into different variables each time it passes through the while loop. Done some prodding around with how to write to a CSV file.

--- vocabgrabby.py
+++ vocabgrabby.py
@@ -1,8 +1,24 @@
 from PyDictionary import PyDictionary
 import os
 import sys
+import csv
+
 sys.stderr = open(os.devnull, 'w')
 
-dictionary = PyDictionary()
-word = input("Enter a word:")
-print(dictionary.meaning(word))
+numcount = int(input("How many terms are in the list you wish to input?"))
+
+
+while numcount > 0:
+    word = input("Please enter a word from the list: ")
+    numcount -= 1
+
+    dictionary = PyDictionary()
+    ##word = input("Enter a word:")
+    print(dictionary.meaning(word))
+
+
+with open('quizletset.csv', 'w', newline='') as f:
+    thewriter = csv.writer(f)
+    #thewriter.writerow([word, dictionary.meaning(word)])
+    for lazy in range(0, numcount):
+        thewriter.writerow([word, dictionary.meaning(word)])
